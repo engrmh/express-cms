88767c998a1670d2e9a3eead660efc8a43c2f6a2
GALAHAD <59095451+engrmh@users.noreply.github.com>
2024-06-04 20:53:23 +0330

    feat: remove and update category

--- controllers/category.js
+++ controllers/category.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const categoryModel = require("../models/category");
 
 exports.create = async (req, res) => {
@@ -22,5 +23,53 @@ exports.getAll = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
-exports.delete = async (req, res) => {};
-exports.update = async (req, res) => {};
+exports.delete = async (req, res) => {
+  try {
+    const isValidID = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (!isValidID) {
+      return res.status(409).json({ message: "User ID Not Valid!!" });
+    }
+
+    const deletedCategory = await categoryModel.findByIdAndRemove({
+      _id: req.params.id,
+    });
+
+    if (!deletedCategory) {
+      return res.status(404).json({ message: "Category Not Found!!" });
+    }
+
+    return res.status(200).json({ message: "Category Deleted Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+exports.update = async (req, res) => {
+  try {
+    const isValidID = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (!isValidID) {
+      return res.status(409).json({ message: "User ID Not Valid!!" });
+    }
+
+    const { title, href } = req.body;
+
+    const updatedCategory = await categoryModel.findByIdAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        $set: {
+          title,
+          href,
+        },
+      }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ message: "Category Not Found!!" });
+    }
+
+    return res.status(200).json({ message: "Category Updated Successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
