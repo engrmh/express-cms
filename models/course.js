a2f4bd4d4154b2014ec41385c3b8d49f8442effa
GALAHAD <59095451+engrmh@users.noreply.github.com>
2024-06-08 22:23:16 +0330

    feat: course model

---
+++ models/course.js
@@ -0,0 +1,67 @@
+const { default: mongoose, models } = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    support: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    creator: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+courseSchema.virtual("sessions", {
+  ref: "Session",
+  localField: "_id",
+  foreignField: "course",
+});
+
+courseSchema.virtual('comments' , {
+    ref: 'Comment',
+    localField: '_id',
+    foreignField: 'course'
+})
+
+const courseModel = mongoose.Model("Course", courseSchema);
+
+module.exports = courseModel;
