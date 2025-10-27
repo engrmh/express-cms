5d463e65f195a1c94489a4309da745212595a96b
Mohammad Hosein Salimbahrami <59095451+engrmh@users.noreply.github.com>
2025-10-27 11:36:28 +0330

    feat: ticket routes

---
+++ controllers/v1/ticket.controller.js
@@ -0,0 +1,49 @@
+exports.getAll = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
+exports.create = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
+exports.userTickets = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
+exports.departments = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
+exports.subDepartments = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
+exports.setAnswer = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
+exports.getOne = async (req, res) => {
+  return res.status(500).json({
+    code: 500,
+    message: "Error been Occurred",
+  });
+};
+
--- routes/v1/ticket.route.js
+++ routes/v1/ticket.route.js
@@ -3,7 +3,25 @@ const ticketController = require("../../controllers/v1/ticket.controller");
 const { authMiddleware } = require("../../middlewares/auth.middeleware");
 const { isAdminMiddleware } = require("../../middlewares/isAdmin.middeleware");
 
-ticketRouter.route("/").get();
-ticketRouter.route("/").get();
+ticketRouter
+  .route("/")
+  .get(authMiddleware, ticketController.getAll)
+  .post(authMiddleware, isAdminMiddleware, ticketController.create);
+
+ticketRouter.route("/user").get(authMiddleware, ticketController.userTickets);
+
+ticketRouter
+  .route("/departments")
+  .get(authMiddleware, ticketController.departments);
+
+ticketRouter
+  .route("/subDepartments/:id")
+  .get(authMiddleware, ticketController.subDepartments);
+
+ticketRouter
+  .route("/answer")
+  .post(authMiddleware, isAdminMiddleware, ticketController.setAnswer);
+
+ticketRouter.route("/:id/answer").get(authMiddleware, ticketController.getOne);
 
 module.exports = ticketRouter;
